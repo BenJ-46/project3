45450e5e41ffcfc99e7aacdae750eaa1b0abd4e3
Dave Trimboli <davidatrimboli@gmail.com>
2020-09-22 19:41:38 -0400

    footer progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,36 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap'
+import Footer from './components/Footer'
+import './App.css'
 const App = () => {
+ 
+
   return (
-    <h1>Hello World!</h1>
+    <>
+    {/* //   <h1>Hello World!</h1> */}
+      <div className="page-container">
+        <div className="content-wrap">
+        
+        </div>
+        <Footer />
+         </div>
+      
+    </>
+    
   )
 }
 
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,2 +1,60 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
 import './Footer.css'
+
+const Footer = () => {
+  return (
+    <>
+    <div className="main-footer center">
+      <div className="container">
+        <div className="row">
+          {/* Column1 */}
+          <div className="col">
+            <h4>Contact</h4>
+            <ul className="list-unstyled">
+              <li>(619) 555-5555</li>
+              <li>San Diego,CA</li>
+              <li>905 California Street</li>
+            </ul>
+         </div>
+          {/* Column2 */}
+          <div className="col">
+            <h4>Resources</h4>
+            <ul className="list-unstyled">
+              <li>Test1</li>
+              <li>Test2</li>
+            </ul>
+          </div>
+          {/* Column3 */}
+          <div className="col">
+            <h4>FAQ'S</h4>
+            <ul className="list-unstyled">
+                <li><Button className="bcolor"  type="button"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.location.href = 'https://guarded-crag-60613.herokuapp.com/';
+                  }}
+                > Accessibility</Button></li>
+              <li>Customer Service</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <p className="col-sm center">
+            &copy;{new Date().getFullYear()} codeIn | All rights reserved | Terms of service | Privacy
+          </p>
+
+        </div>
+
+      </div>
+
+    </div>
+</>
+
+  )
+}
+
+export default Footer
