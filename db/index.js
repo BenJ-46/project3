ebb28146c3543a3749dd3032d6f3ef6eadfb0887
Kalyn Simms <Kalynbsimms@gmail.com>
2020-09-22 13:49:47 -0700

    updated database

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 // replace <db_name> with your database
-module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/<db_name>', {
+module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/codeIn_db', {
   useNewUrlParser: true,
   useFindAndModify: true,
   useCreateIndex: true,
