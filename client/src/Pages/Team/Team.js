aac6c9a087a19f7635518afa3715586a6900258c
Dave Trimboli <davidatrimboli@gmail.com>
2020-09-24 00:05:27 -0400

    centered text

--- client/src/Pages/Team/Team.js
+++ client/src/Pages/Team/Team.js
@@ -13,33 +13,33 @@ const Team = () => {
       <Row>
       <Col sm="3">
         <Card body>
-          <CardImg  className="sizing" top width="100%" src={Kalyn} alt="Card image cap" />
-          <CardTitle>Kalyn Simms</CardTitle>
-          <CardText>Stuff about Kalyn</CardText>
+          <CardImg top width="100%" src={Kalyn} alt="Card image cap" />
+            <CardTitle className="center">Kalyn Simms</CardTitle>
+            <CardText className="center">Stuff about Kalyn</CardText>
         
         </Card>
       </Col>
         <Col sm="3">
           <Card body>
             <CardImg  top width="100%" src={Nic} alt="Card image cap" />
-            <CardTitle>Nicholas Dallas</CardTitle>
-            <CardText>Stuff about Nic</CardText>
+            <CardTitle className="center">Nicholas Dallas</CardTitle>
+            <CardText className="center">Stuff about Nicholas</CardText>
             
           </Card>
         </Col>
         <Col sm="3">
           <Card body>
             <CardImg  top width="100%" src={Dave} alt="Card image cap" />
-            <CardTitle>Dave Trimboli</CardTitle>
-            <CardText>Stuff about Dave</CardText>
+            <CardTitle className="center">Dave Trimboli</CardTitle>
+            <CardText className="center">Stuff about Dave</CardText>
 
           </Card>
         </Col>
         <Col sm="3">
           <Card body>
             <CardImg top width="100%" src={Ben} alt="Card image cap" />
-            <CardTitle>Benjamin Jimenez</CardTitle>
-            <CardText>Stuff about Benjamin</CardText>
+            <CardTitle className="center">Benjamin Jimenez</CardTitle>
+            <CardText className="center">Stuff about Benjamin</CardText>
 
           </Card>
         </Col>
