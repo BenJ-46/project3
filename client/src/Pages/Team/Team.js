b9e99d1ef995ee33d33642477aa95cac3deed7b6
Dave Trimboli <davidatrimboli@gmail.com>
2020-09-23 23:51:17 -0400

    team page cropped images

--- client/src/Pages/Team/Team.js
+++ client/src/Pages/Team/Team.js
@@ -1,45 +1,45 @@
 import React from 'react'
 import { Card, CardImg, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
-import Kalyn from '../../components/Assets/Images/kalyn.jpg'
-import Nic from '../../components/Assets/Images/nic.jpg'
-import Dave from '../../components/Assets/Images/dave.jpg'
-import Ben from '../../components/Assets/Images/ben.jpg'
+import Kalyn from '../../components/Assets/Images/kalyncrop.png'
+import Nic from '../../components/Assets/Images/nic4.png'
+import Dave from '../../components/Assets/Images/davecrop9.png'
+import Ben from '../../components/Assets/Images/bencrop.png'
 import './Team.css'
 
 const Team = () => {
   return (
     <>
-      <h1 className="center">Get to know our team</h1>
+      <h1 className="center font-weight-bold" >get To Know Our Team</h1>
       <Row>
       <Col sm="3">
         <Card body>
-          <CardImg   top width="100%" src={Kalyn} alt="Card image cap" />
-          <CardTitle className="center font">Kalyn Simms</CardTitle>
-          <CardText className="center font2">Stuff about Kalyn</CardText>
+          <CardImg  className="sizing" top width="100%" src={Kalyn} alt="Card image cap" />
+          <CardTitle>Kalyn Simms</CardTitle>
+          <CardText>Stuff about Kalyn</CardText>
         
         </Card>
       </Col>
         <Col sm="3">
           <Card body>
             <CardImg  top width="100%" src={Nic} alt="Card image cap" />
-            <CardTitle className="center font">Nicholas Dallas</CardTitle>
-            <CardText className="center font2">Stuff about Nic</CardText>
+            <CardTitle>Nicholas Dallas</CardTitle>
+            <CardText>Stuff about Nic</CardText>
             
           </Card>
         </Col>
         <Col sm="3">
           <Card body>
-            <CardImg top width="100%" src={Dave} alt="Card image cap" />
-            <CardTitle className="center font">Dave Trimboli</CardTitle>
-            <CardText className="center font2">Stuff about Dave</CardText>
+            <CardImg  top width="100%" src={Dave} alt="Card image cap" />
+            <CardTitle>Dave Trimboli</CardTitle>
+            <CardText>Stuff about Dave</CardText>
 
           </Card>
         </Col>
         <Col sm="3">
           <Card body>
             <CardImg top width="100%" src={Ben} alt="Card image cap" />
-            <CardTitle className="center font">Benjamin Jimenez</CardTitle>
-            <CardText className="center font2">Stuff about Benjamin</CardText>
+            <CardTitle>Benjamin Jimenez</CardTitle>
+            <CardText>Stuff about Benjamin</CardText>
 
           </Card>
         </Col>
