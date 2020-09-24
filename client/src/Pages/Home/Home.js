86888eb2d426f7a8ea852fb743d4e4f1484909a3
Kalyn Simms <Kalynbsimms@gmail.com>
2020-09-24 14:41:37 -0700

    code

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -8,29 +8,34 @@ const Home = () => {
           display: "flex",
           justifyContent: "space-around",
           margin:"18px 0px",
+          paddingBottom: "18px",
           borderBottom:"1px solid grey"
         }}>
 
         
           <div>
             <img style={{ width: "160px", height: "160px", borderRadius: "80px" }} src="https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Man"/>
-          </div>
-          <div>
             <h4>Bart Simpson</h4>
+          </div>
+          
+          
+            <div>
             <div style={{display: "flex", justifyContent:"space-between", width:"108%"}} >
               <h6>4 Repositories</h6>
               <h6>100 Collabs</h6>
-              <h6>50 Followers  </h6>
+              <h6>50 Projects </h6>
 
-              
-            </div>
+              </div>
+            
             <div>
               <h5>Hello World</h5>
             </div>
           </div>
         </div>
       </div>
-
+      <div>
+        <b><h3>Bio</h3></b>
+      </div>
 
     </>
   )
