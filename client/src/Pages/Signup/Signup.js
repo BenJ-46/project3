980f6213399156c2b4be767d8619f8ecde9c30e0
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-09-25 12:01:10 -0700

    imported axios

--- client/src/Pages/Signup/Signup.js
+++ client/src/Pages/Signup/Signup.js
@@ -6,6 +6,7 @@ import { FacebookLoginButton } from 'react-social-login-buttons';
 import { InstagramLoginButton } from 'react-social-login-buttons';
 import './Signup.css'
 import { Router } from 'react-router-dom';
+import axios from 'axios'
 
 
 const Signup = () => {
@@ -35,6 +36,7 @@ const Signup = () => {
       language: userState.language,
       devHistory: userState.history
     }).then( data => {
+
       localStorage.setItem('user', userState.name)
       window.location = '/Profile'
     })
