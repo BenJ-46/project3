7d0de84cf7b7cc00edc96e7663112428e01092a9
Kalyn Simms <Kalynbsimms@gmail.com>
2020-09-28 11:44:36 -0700

    updated cards on the collab page

--- client/src/Pages/Collab/Collab.js
+++ client/src/Pages/Collab/Collab.js
@@ -3,9 +3,11 @@
 
 
 import React from "react";
- import Carousel from "@brainhubeu/react-carousel";
- import "@brainhubeu/react-carousel/lib/style.css";
- import { Card } from "reactstrap"
+import "@brainhubeu/react-carousel/lib/style.css";
+import Carousel, { Dots } from '@brainhubeu/react-carousel';
+import { Card ,  Button} from "reactstrap"
+import { Cell } from "react-mdl"
+import './Collab.css'
 
  import Image1 from "../../components/Assets/Images/nic4.png";
  import Image2 from "../../components/Assets/Images/bencrop.png";
@@ -13,38 +15,102 @@ import React from "react";
 
 
 
- const Portfolio = () => (
+ const Portfolio = () => {
+
+  return (
+    <>
    <div
      className="App"
      style={{ width: "1000px", margin:"auto", padding: "50px" }}
    >
-{/* <Carousel arrows infinite>
-      <Card>
-        <img src={Image1} />
-      </Card>
-    </Carousel> */}
+
  <Carousel arrows infinite>
    <Card>
-     <img style={{ height: "600px", paddingtop:"5em", padding:"1em", margin:"auto"}}
+     <img style={{ height: "400px", paddingtop:"5em", padding:"1em", margin:"auto"}}
        src="https:images.unsplash.com/photo-1592158169526-9deda479afce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=783&q=80"
        alt="avatar"
        className="avatar-img" />
 
-    <h1>Hello World</h1>
-   </Card>
+    <h1>Lisa Simpson</h1>
+    <h2><b><span>Language</span></b></h2>
+    <h3>Username</h3>
+    <h3>bio</h3>
+         <Button color="warning" size="lg" block>Let's Collab</Button>
+         <Button color="warning" size="lg" block>Maybe Next time!</Button>
 
-   <Card>
-     <img src={Image3} />
+       
    </Card>
+       <Card>
+         <img style={{ height: "400px", paddingtop: "5em", padding: "1em", margin: "auto" }}
+           src="https:images.unsplash.com/photo-1592158169526-9deda479afce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=783&q=80"
+           alt="avatar"
+           className="avatar-img" />
+
+         <h1>Bart Simpson</h1>
+         <h2><b><span>Language</span></b></h2>
+         <h3>Username</h3>
+         <h3>bio</h3>
+         <Button color="warning" size="lg" block>Let's Collab</Button>
+         <Button color="warning" size="lg" block>Maybe Next time!</Button>
+
+
+       </Card>
+       <Card>
+         <img style={{ height: "400px", paddingtop: "5em", padding: "1em", margin: "auto" }}
+           src="https:images.unsplash.com/photo-1592158169526-9deda479afce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=783&q=80"
+           alt="avatar"
+           className="avatar-img" />
+
+         <h1>Homer Simpson</h1>
+         <h2><b><span>Language</span></b></h2>
+         <h3>Username</h3>
+         <h3>bio</h3>
+         <Button color="warning" size="lg" block>Let's Collab</Button>
+         <Button color="warning" size="lg" block>Maybe Next time!</Button>
+
+
+       </Card>
+       <Card>
+         <img style={{ height: "400px", paddingtop: "5em", padding: "1em", margin: "auto" }}
+           src="https:images.unsplash.com/photo-1592158169526-9deda479afce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=783&q=80"
+           alt="avatar"
+           className="avatar-img" />
+
+         <h1>Marge Simpson</h1>
+         <h2><b><span>Language</span></b></h2>
+         <h3>Username</h3>
+         <h3>bio</h3>
+         <Button color="warning" size="lg" block>Let's Collab</Button>
+         <Button color="warning" size="lg" block>Maybe Next time!</Button>
+
+
+       </Card>
+       <Card>
+         <img style={{ height: "400px", paddingtop: "5em", padding: "1em", margin: "auto" }}
+           src="https:images.unsplash.com/photo-1592158169526-9deda479afce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=783&q=80"
+           alt="avatar"
+           className="avatar-img" />
+
+         <h1>santa's little helper Simpson</h1>
+         <h2><b><span>Language</span></b></h2>
+         <h3>Username</h3>
+         <h3>bio</h3>
+         <Button color="warning" size="lg" block>Let's Collab</Button>
+         <Button color="warning" size="lg" block>Maybe Next time!</Button>
+
+
+       </Card>
+
+   
  </Carousel>
 
 
 
-{/* <img src={Image2} />
-      <img src={Image3} />
-    </Carousel> */}
+
    </div>
- );
+   </>
+  )
+  }
 
  export default Portfolio;
 
