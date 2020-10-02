7abc7dc1b70e85a355eda70ae183a073ce94a85b
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-10-01 19:07:07 -0700

    deleted

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,31 +1,31 @@
 const router = require('express').Router()
 const { User } = require('../models')
-const passport = require('passport')
-const jwt = require('jsonwebtoken')
+// const passport = require('passport')
+// const jwt = require('jsonwebtoken')
  
 // Create One user to Register
-router.post('/users/register', (req, res) => {
-  const { name, username, email, bio, devHist, language, password } = req.body
-  User.register(new User({ name, username, email, bio, devHist, language }), password, err => {
-    if (err) { console.log(err) }
-    res.sendStatus(200)
-  })
-})
+// router.post('/users/register', (req, res) => {
+//   const { name, username, email, bio, devHist, language, password } = req.body
+//   User.register(new User({ name, username, email, bio, devHist, language }), password, err => {
+//     if (err) { console.log(err) }
+//     res.sendStatus(200)
+//   })
+// })
 
 // User Login Route w/ Authorization
-router.post('/users/login', (req, res) => {
-  const { email, password } = req.body
-  User.authenticate()(email, password, (err, user) => {
-    if (err) { console.log(err) }
-    res.json(user ? jwt.sign({ id: user._id }, process.env.SECRET) : null)
-  })
-})
+// router.post('/users/login', (req, res) => {
+//   const { email, password } = req.body
+//   User.authenticate()(email, password, (err, user) => {
+//     if (err) { console.log(err) }
+//     res.json(user ? jwt.sign({ id: user._id }, process.env.SECRET) : null)
+//   })
+// })
 
 // this gives the jsonWebToken
-router.get('/users', passport.authenticate('jwt'), (req, res) => {
-  res.sendStatus(200)
-  res.json(req.user)
-})
+// router.get('/users', passport.authenticate('jwt'), (req, res) => {
+//   res.sendStatus(200)
+//   res.json(req.user)
+// })
 
 
 // CodeIn ROUTES //
@@ -42,6 +42,11 @@ router.get('/users/:name', (req, res) => {
     .catch(err => console.error(err))
 })
 
+router.post('/users', (req, res) => {
+  User.create(req.body)
+    .then(user => res.json(user))
+    .catch(err => console.error(err))
+})
 
 // POST one User/:name & relation it w/ collabList
 router.post('/users/:name', (req, res) => {
@@ -61,6 +66,4 @@ router.post('/users/:name', (req, res) => {
     });
 })
 
-
 module.exports = router
-
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-require('dotenv').config()
+// require('dotenv').config()
 const express = require('express')
 const { join } = require('path')
-// bring in passport
-const passport = require('passport')
-// bring in possport local strategy 'propt' 
-const { Strategy } = require('passport-local')
-// Need Strategy and Extract of JWT
-const { Strategy: JWTStrategy, ExtractJwt} = require('passport-jwt')
-// bring in user model from models folder
-const { User } = require('./models')
+
+// // bring in passport
+// const passport = require('passport')
+// // bring in possport local strategy 'propt' 
+// const { Strategy } = require('passport-local')
+// // Need Strategy and Extract of JWT
+// const { Strategy: JWTStrategy, ExtractJwt} = require('passport-jwt')
+// // bring in user model from models folder
+// const { User } = require('./models')
 
 const app = express()
 
@@ -16,25 +17,24 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(join(__dirname, 'client', 'build')))
 }
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
 // PASSPORT MIDDLE WARE //
-app.use(passport.initialize())
-app.use(passport.session())
+// app.use(passport.initialize())
+// app.use(passport.session())
 // rules of Passport itself && use auth off of User Model
-passport.use(new Strategy(User.authenticate()))
+// passport.use(new Strategy(User.authenticate()))
 // Serialize && Deserialize 1 USER
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+// passport.serializeUser(User.serializeUser())
+// passport.deserializeUser(User.deserializeUser())
 
-passport.use(new JWTStrategy({
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: process.env.SECRET
-}, ({ id }, cb) => User.findById(id)
-// .populate('collabList')
-.then(user => cb(null, user))
-.catch(err => cb(err))
-))
+// passport.use(new JWTStrategy({
+//   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+//   secretOrKey: process.env.SECRET
+// }, ({ id }, cb) => User.findById(id)
+// .then(user => cb(null, user))
+// .catch(err => cb(err))
+// ))
 
 app.use(require('./routes'))
 
